c2f105aca22ac5a657ef5063fe0431b97a2f8742
Megh Patel <f20180148@goa.bits-pilani.ac.in>
2022-07-28 22:23:38 +0530

    Added dynamic disk loading & checkpoint features

--- README.md
+++ README.md
@@ -2,30 +2,20 @@
 
 To run the U-Net:
 
-1. Create a folder data in the same directory as other files
+1. Create data folder where unet.py is present and create subfolders './data/results', './data/train' and './data/test'
 
-2. Create folders npydata, results, train and test inside data folder
-
-3. Create folders images and labels inside both the train and test folders. All images must of type png
-
-4. Place your training images and their labels(mask) inside './data/train/images' and './data/train/labels' and place your testing images under './data/test/images'
-
-5. Execute 'data.py' to process dataset images and labels. You can check available script options using 'python data.py -h'. Example-
+2. Place your training images and their labels(mask) inside './data/train/images' and './data/train/labels' respectively and place your testing images under './data/test/images'. All images must be of type png
+   
+3. Execute 'unet.py' with 'train' operation to train your dataset. You can check available script options using 'python unet.py -h'. Example-
    
    ```bash
-   python data.py --height 256 --width 256
+   python unet.py --operation train --batch_size 16 --epochs 50 --image_size 512
    ```
 
-6. Execute 'unet.py' with 'train' operation to train your dataset. You can check available script options using 'python unet.py -h'. Example-
+4. Once training is complete, you can get results by executing 'unet.py' in 'test' operation. Results will be placed under './data/results'
    
    ```bash
-   python unet.py --operation train --batch_size 16 --epochs 50
+   python unet.py --operation test --batch_size 16 --image_size 512
    ```
 
-7. Once training is complete, you can get results by executing 'unet.py' in 'test' operation. Results will be placed under './data/results'
-   
-   ```bash
-   python unet.py --operation test
-   ```
-
-8. Trained weights can be found in the checkpoint directory - 'unet.hdf5'
+5. Trained weights can be found in the checkpoint directory - 'unet.hdf5'
--- unet.py
+++ unet.py
@@ -1,21 +1,27 @@
 # -*- coding:utf-8 -*-
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+from tensorflow.keras.preprocessing.image import array_to_img, ImageDataGenerator
+from random import randint
 
-from keras.models import *
-from keras.layers import *
-from keras.optimizers import *
-from keras.callbacks import ModelCheckpoint
-from keras.utils import array_to_img
-from keras.callbacks import CSVLogger
-
+import tensorflow as tf
+import numpy as np
 import cv2, argparse
-from data import *
+import os, glob, math
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.autograph.set_verbosity(1)
 
 def argparser():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description = 'U-NET implementation using Keras')
-    parser.add_argument('--operation', type=str, default='train', help='whether \'train\' or \'test\'', metavar='')
+    parser.add_argument('--operation', type=str, required=True, help='whether \'train\' or \'test\'', metavar='')
     parser.add_argument('--checkpoint_dir', type=str, default='.', help='where to save trained model, parameters and logs', metavar='')
-    parser.add_argument('--batch_size', type=int, default=64, help='batch size for training the model', metavar='')
-    parser.add_argument('--epochs', type=int, default=100, help='number of epochs for training the model', metavar='')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size for training the model', metavar='')
+    parser.add_argument('--image_size', type=int, default=256, help='image will be resized to (size X size) pixels', metavar='')
+    parser.add_argument('--epochs', type=int, default=50, help='number of epochs for training the model', metavar='')
+    parser.add_argument('--initial_epoch', type=int, default=1, help='starting epoch for continuing model training', metavar='')
     parser.add_argument('--validation_split', type=float, default=0.1, help='fraction of training data to be used for validation', metavar='')
     parser.add_argument('--shuffle', action='store_true', help='whether to shuffle the training data before each epoch')
     parser.add_argument('--continue_training', action='store_true', help='continue training the previous model')
@@ -34,53 +40,48 @@ def print_params(args):
         with open(args.checkpoint_dir + '/train_params.txt', 'wt') as opt_file:
             opt_file.write(message)
 
+class ImageCallback(tf.keras.callbacks.Callback):
+    def __init__(self, checkpoint_dir, validation_generator):
+        self.img_dir = checkpoint_dir + '/images/'
+        self.generator = validation_generator
+        try: os.mkdir(self.img_dir)
+        except OSError: pass
+
+    def on_epoch_end(self, epoch, logs={}):
+        validation_data = next(self.generator)
+        idx = randint(1, len(validation_data[0])) - 1
+        x_test = validation_data[0][idx]
+        y_test = validation_data[1][idx]
+        y_pred = self.model.predict(np.reshape(x_test, (1,) + x_test.shape), verbose=0)[0]
+        array_to_img(x_test).save(self.img_dir + '/epoch{0:03d}_real_A.png'.format(epoch+1))
+        array_to_img(y_test).save(self.img_dir + '/epoch{0:03d}_real_B.png'.format(epoch+1))
+        array_to_img(y_pred).save(self.img_dir + '/epoch{0:03d}_fake_B.png'.format(epoch+1))
+
 class myUnet(object):
-    def __init__(self):
-        train_shape = np.load('./data/npydata/imgs_test.npy').shape
-        self.img_rows = train_shape[1]
-        self.img_cols = train_shape[2]
+    def __init__(self, _image_size):
+        self.imgage_size = _image_size
         self.get_unet()
 
-    def load_train_data(self):
-        mydata = dataProcess(self.img_rows, self.img_cols)
-        imgs_train, imgs_mask_train = mydata.load_train_data()
-        return imgs_train, imgs_mask_train
-
-    def load_test_data(self):
-        mydata = dataProcess(self.img_rows, self.img_cols)
-        imgs_test = mydata.load_test_data()
-        return imgs_test
-
     def get_unet(self):
-        inputs = Input((self.img_rows, self.img_cols, 3))
+        inputs = Input((self.imgage_size, self.imgage_size, 3))
 
         conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
-        # print(conv1)
         conv1 = BatchNormalization()(conv1)
-        print ("conv1 shape:", conv1.shape)
         conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
         conv1 = BatchNormalization()(conv1)
-        print ("conv1 shape:", conv1.shape)
         pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-        print ("pool1 shape:", pool1.shape)
 
         conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
-        print ("conv2 shape:", conv2.shape)
         conv2 = BatchNormalization()(conv2)
         conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
-        print ("conv2 shape:", conv2.shape)
         conv2 = BatchNormalization()(conv2)
         pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-        print ("pool2 shape:", pool2.shape)
 
         conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
-        print ("conv3 shape:", conv3.shape)
         conv3 = BatchNormalization()(conv3)
         conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
-        print ("conv3 shape:", conv3.shape)
         conv3 = BatchNormalization()(conv3)
         pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-        print ("pool3 shape:", pool3.shape)
 
         conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
         conv4 = BatchNormalization()(conv4)
@@ -98,25 +99,17 @@ class myUnet(object):
         up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
         up6 = BatchNormalization()(up6)
         merge6 = concatenate([drop4, up6], axis=3)
-        print(up6)
-        print(merge6)
         conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
-        print(conv6)
         conv6 = BatchNormalization()(conv6)
         conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
-        print(conv6)
         conv6 = BatchNormalization()(conv6)
         up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
             UpSampling2D(size=(2, 2))(conv6))
         up7 = BatchNormalization()(up7)
         merge7 = concatenate([conv3, up7], axis=3)
-        print(up7)
-        print(merge7)
         conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
         conv7 = BatchNormalization()(conv7)
-        print(conv7)
         conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
-        print(conv7)
         conv7 = BatchNormalization()(conv7)
         up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
             UpSampling2D(size=(2, 2))(conv7))
@@ -130,80 +123,121 @@ class myUnet(object):
             UpSampling2D(size=(2, 2))(conv8))
         up9 = BatchNormalization()(up9)
         merge9 = concatenate([conv1, up9], axis=3)
-        print(up9)
-        print(merge9)
         conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
         conv9 = BatchNormalization()(conv9)
-        print(conv9)
         conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
         conv9 = BatchNormalization()(conv9)
-        print(conv9)
         conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
         conv9 = BatchNormalization()(conv9)
-        print ("conv9 shape:", conv9.shape)
 
         conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
-        print(conv10)
         model = Model(inputs=inputs, outputs=conv10)
 
         model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
 
         self.model = model
 
-    def train(self, checkpoint_dir, continue_training, _batch_size, _epochs, _verbose, _validation_split, _shuffle):
-        print("loading data")
-        imgs_train, imgs_mask_train = self.load_train_data()
-        print("loading data done")
-        self.load_model_weights(checkpoint_dir, continue_training)
-        model_checkpoint = ModelCheckpoint(checkpoint_dir + '/unet.hdf5', monitor='val_loss', verbose=0, save_best_only=True)
-        csv_logger = CSVLogger(checkpoint_dir + '/model_history_log.csv', append=continue_training)
+    def train(self, checkpoint_dir, continue_training, _batch_size, _epochs, initial_epoch, _verbose, _validation_split, _shuffle):
+        if continue_training:
+            self.model.load_weights(checkpoint_dir + '/unet.hdf5')
+        elif os.path.exists(checkpoint_dir + '/unet.hdf5'):
+            os.remove(checkpoint_dir + '/unet.hdf5')
+        
+        image_generator = ImageDataGenerator(rescale = (1/255), validation_split = _validation_split)
+        _seed = randint(100,100000)
+        train_image_generator = image_generator.flow_from_directory(
+            './data/train/',
+            classes = ['images'],
+            class_mode=None,
+            target_size = (self.imgage_size, self.imgage_size), 
+            color_mode = 'rgb', 
+            batch_size = _batch_size, 
+            shuffle = _shuffle,
+            seed = _seed,
+            subset = 'training',
+        )
+        train_label_generator = image_generator.flow_from_directory(
+            './data/train/',
+            classes = ['labels'],
+            class_mode=None,
+            target_size = (self.imgage_size, self.imgage_size), 
+            color_mode = 'grayscale', 
+            batch_size = _batch_size, 
+            shuffle = _shuffle,
+            seed = _seed,
+            subset = 'training',
+        )
+        _seed = randint(100,100000)
+        validation_image_generator = image_generator.flow_from_directory(
+            './data/train/',
+            classes = ['images'],
+            class_mode=None,
+            target_size = (self.imgage_size, self.imgage_size), 
+            color_mode = 'rgb', 
+            batch_size = _batch_size, 
+            shuffle = _shuffle,
+            seed = _seed,
+            subset = 'validation',
+        )
+        validation_label_generator = image_generator.flow_from_directory(
+            './data/train/',
+            classes = ['labels'],
+            class_mode=None,
+            target_size = (self.imgage_size, self.imgage_size), 
+            color_mode = 'grayscale', 
+            batch_size = _batch_size, 
+            shuffle = _shuffle,
+            seed = _seed,
+            subset = 'validation',
+        )
+        train_generator = zip(train_image_generator, train_label_generator)
+        validation_generator = zip(validation_image_generator, validation_label_generator)
+        
+        model_checkpoint = ModelCheckpoint(checkpoint_dir + '/unet.hdf5', monitor='loss', verbose=0, save_best_only=True)
+        tensorboard_callback = TensorBoard(log_dir=checkpoint_dir+'/tensorboard_logs/', histogram_freq=1)
+        
         print('Fitting model...')
-        self.model.fit(x=imgs_train, y=imgs_mask_train, batch_size=_batch_size, epochs=_epochs, verbose=_verbose,
-                  validation_split=_validation_split, shuffle=_shuffle , callbacks=[model_checkpoint, csv_logger])
+        self.model.fit(train_generator, 
+            epochs=_epochs + (initial_epoch-1), 
+            verbose=_verbose, 
+            steps_per_epoch=train_image_generator.samples // _batch_size,
+            validation_data=validation_generator,
+            validation_steps=validation_image_generator.samples // _batch_size,
+            callbacks=[model_checkpoint, tensorboard_callback, ImageCallback(checkpoint_dir, validation_generator)],
+            initial_epoch=(initial_epoch-1),
+        )
         self.model.save_weights(checkpoint_dir + '/unet.hdf5')
         
-    def test(self, checkpoint_dir):
-        print("loading data")
-        imgs_test = self.load_test_data()
-        print("loading data done")
+    def test(self, checkpoint_dir, _batch_size):
+        test_generator = ImageDataGenerator(rescale = (1/255)).flow_from_directory(
+            './data/test/',
+            classes = ['images'],
+            target_size = (self.imgage_size, self.imgage_size), 
+            color_mode = 'rgb',
+            shuffle=False,
+            batch_size = _batch_size,
+        )
         print('predict test data')
         self.model.load_weights(checkpoint_dir + '/unet.hdf5')
-        imgs_mask_test = self.model.predict(imgs_test, batch_size=1, verbose=1)
+        num_images = len(glob.glob('./data/test/images/*.png'))
+        _steps = math.ceil(num_images / _batch_size)
+        imgs_mask_test = self.model.predict(test_generator, steps=_steps, verbose=1)
+        
         print("Saving images")
-        np.save('./data/results/imgs_mask_test.npy', imgs_mask_test)
-        piclist = []
-        for line in open("./data/results/pic.txt"):
-            line = line.strip()
-            picname = line.split(os.sep)[-1]
-            piclist.append(picname)
-        print(len(piclist))
+        piclist = [i.split(os.sep)[-1] for i in glob.glob('./data/test/images/*.png')]
+        test_img_shape = cv2.imread(glob.glob('./data/test/images'+os.sep+'*.png')[0]).shape
+        height, width = test_img_shape[0], test_img_shape[1]
         for i in range(imgs_mask_test.shape[0]):
             path = "./data/results/" + piclist[i]
-            img = imgs_mask_test[i]
-            img = array_to_img(img)
-            img.save(path)
-            train_img = cv2.imread(glob.glob('./data/train/labels'+os.sep+'*.png')[0], cv2.IMREAD_GRAYSCALE)
-            height, width = train_img.shape
-            cv_pic = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-            cv_pic = cv2.resize(cv_pic,(width,height))
-            binary, cv_save = cv2.threshold(cv_pic, 127, 255, cv2.THRESH_BINARY)
-            cv2.imwrite(path, cv_save)
-            
-    def load_model_weights(self, checkpoint_dir, continue_training):
-        weight_file = checkpoint_dir + '/unet.hdf5'
-        if continue_training:
-            self.model.load_weights(weight_file)
-        else:
-            if os.path.exists(weight_file):
-                os.remove(weight_file)
+            array_to_img(imgs_mask_test[i]).resize((width,height)).save(path)
 
 if __name__ == '__main__':
     args = argparser()
     print_params(args)
-    myunet = myUnet()
+    myunet = myUnet(args.image_size)
     if args.operation == 'train':
-        myunet.train(args.checkpoint_dir, args.continue_training, args.batch_size, args.epochs, args.verbose, args.validation_split, args.shuffle)
+        myunet.train(args.checkpoint_dir, args.continue_training, args.batch_size, args.epochs, args.initial_epoch, args.verbose, args.validation_split, args.shuffle)
     elif args.operation == 'test':
-        myunet.test(args.checkpoint_dir)
+        myunet.test(args.checkpoint_dir, args.batch_size)
     else:
         print('ERROR : INVALID OPERATION')
